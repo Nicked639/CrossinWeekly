1eb3b2ffdbfa77973dd171cd78eeeee7d7a0ef18
nicktimebreak <shxnick@gmail.com>
2017-04-28 17:44:12 +0800

    fujia

--- MaxProduct.py
+++ MaxProduct.py
@@ -1,15 +1,16 @@
-def product(num):
-  n = str(num)
-  max = 1
-  i = 1
-  while i < len(n):
-      if i == 0:
-          p = int(n[0]) * int(n[1:])
-      else:
-          p = int(n[0:i]) * int(n[i:])
-      if max < p:
-          max = p
-      i += 1
-  return max
 
-print(product(12345))
+import itertools
+def product(num):
+    nn = str(num)
+    max = 1
+    for t in list(itertools.permutations(nn,len(nn))):
+        n = "".join(t)
+        i = 1
+        while i < len(n):
+            p = int(n[0:i]) * int(n[i:])
+            if max < p:
+                max = p
+            i += 1
+    return max
+
+print(product(123456))
